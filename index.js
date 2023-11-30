e3ccad2a5aa249d6d09ff7f272917af859434791
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2023-11-30 03:24:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+alert("Welcome to tutorial-54 of Sigma batch CodeWithHarry "); // This is frontend as whenever we click on our page we get an 'alert' notification.
+
+// JS is a client-side scripting language.
+// JS provide us functionality.
+
+console.log("JS can be used for both frontend and backend development.");
+
+console.log("JS can be used at the backened for e.g we can use JS script at the server side for runing some queries which we gonna to run through MongoDB.");
+
+console.log("\n");
+console.log("JS written for the front-end simply by runing like this code in JS file which we had linked to HTML doc.");
+
+console.log("This is how js is written for frontend");
+
+console.log("\n");
+console.log("By runing the JS at backened we can design the entire backened.");
+console.log("with JS we can do server site scripting i.e. server par query run kar saktey hain, wahan se data pull kar k client ko send kar saktey hain")
+
+console.log("Modern JS is so awsome as we have no. of good functions like map, filter, reduce etc and with the help of these functions we can do scripting very easily and will enjoy the code also.");
+
+console.log("\n");
+
+console.log("In console.log '.log' is a function which can log the data inside console window.");
+
+console.log(`prompt -> It is a way of taking input from user`);
+let name = prompt("Enter your full name:");
+console.log(`name is ${name}`);
+let age = prompt("Enter your age");
+console.log(`Age is ${age}`);
+
+console.log("After alert and prompt we also have another function and that's confirm");
+
+let dine = confirm("Are you comming for dinner");
+let dinner = prompt("");
+let dec = confirm("Are you sur you don't wanna to come along");
+let decFinal = prompt("");
+console.log(decFinal);
+
+console.log("\n");
+console.log("Let's install NodeJS");
+
+console.log("What i Nodejs ?");
+console.log("Nodejs is a v8 engine of JS jo ki chrome use karta hain for runing our script.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ new.js
@@ -0,0 +1,60 @@
+// JS is a client-side scripting language.
+// JS provide us functionality.
+
+console.log("JS can be used for both frontend and backend development.");
+
+console.log("JS can be used at the backened for e.g we can use JS script at the server side for runing some queries which we gonna to run through MongoDB.");
+
+console.log("\n");
+console.log("JS written for the front-end simply by runing like this code in JS file which we had linked to HTML doc.");
+
+console.log("This is how js is written for frontend");
+
+console.log("\n");
+console.log("By runing the JS at backened we can design the entire backened.");
+console.log("with JS we can do server site scripting i.e. server par query run kar saktey hain, wahan se data pull kar k client ko send kar saktey hain")
+
+console.log("Modern JS is so awsome as we have no. of good functions like map, filter, reduce etc and with the help of these functions we can do scripting very easily and will enjoy the code also.");
+
+console.log("\n");
+
+console.log("In console.log '.log' is a function which can log the data inside console window.");
+
+console.log(`prompt -> It is a way of taking input from user`);
+
+console.log("We are opening the new.js in terminal");
+
+console.log("Jo bhi kaam main kar rha tha apne broser ki console window k oopar, ab main wo sarey kaam kar sakta hu new.js k ander apne system mein, means js ki practice kar sakta hu iss 'node' k through.");
+
+console.log("We can create multiple software with the help of Nodejs. There are endless posibility of Nodejs");
+
+console.log("we will develop the backened also with the help of Nodejs in future.");
+
+console.log("Frontend mein js ko use karne k liye we will use console window in our browser.");
+console.log("For using JS in the backened, we will use nodejs t orun our program.");
+
+console.log("There are obviously some difference between js for rontend and js for backened.");
+console.log("Like in frontend js we have no. of functions for using such as prompt, confirm, alert etc which we didn't encouter in backened js or in Nodejs");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
